50e2923c1a95df6ee05a7b664f60cb57cee7a760
enerzilius <efblouback@gmail.com>
2025-12-30 17:38:15 -0300

    renomeando

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ render.height = 1000;
 const context = render.getContext("2d");
 
 // Canvas Attributes
-const HEIGHT = 1000;
-const WIDTH = 1000;
-const COLOR = "#EEEEEE";
+const canvasHeight = 1000;
+const canvasWidth = 1000;
+const backgroundColor = "#EEEEEE";
 const borderWidth = 10;
 
-context.fillStyle = COLOR;
-context.fillRect(0, 0, WIDTH, HEIGHT);
+context.fillStyle = backgroundColor;
+context.fillRect(0, 0, canvasWidth, canvasHeight);
 
 // Point
 
@@ -19,6 +19,6 @@ const pointLength = 50;
 const pointColor = "#FD11FD";
 
 context.fillStyle = pointColor;
-context.fillRect(WIDTH/2 - pointLength/2, HEIGHT/2 - pointLength/2, pointLength, pointLength);
+context.fillRect(canvasWidth/2 - pointLength/2, canvasHeight/2 - pointLength/2, pointLength, pointLength);
 
 console.log(context);
